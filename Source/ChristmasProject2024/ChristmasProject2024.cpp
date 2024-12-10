6cce1fb285b681314eba710211bc55d0a65d8420
Charlie Campbell <chacha9669@hotmail.com>
2024-12-10 14:51:09 +1100

    Enemy Class follows player pawn

---
+++ Source/ChristmasProject2024/ChristmasProject2024.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChristmasProject2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ChristmasProject2024, "ChristmasProject2024" );
